1a4429babb5e3cfff175497315b1b82190da1ab8
Quy HM <quyhm@vitalify.asia>
2021-12-26 17:32:23 +0700

    explain

--- longest-consecutive.js
+++ longest-consecutive.js
@@ -16,3 +16,20 @@
     }
     return res;
 };
+
+//Input: nums = [100,4,200,1,3,2]
+//Output: 4
+
+//Input: nums = [0,3,7,2,5,8,4,6,0,1]
+//Output: 9
+
+/**
+ * The idea is
+ * Find the number less than the current number, if there exists a smaller number, I do nothing, 
+ * this means I only need to check smaller number.
+ * If there is no smaller number than the current number, I will store that number in a temporary variable.
+ * Check whether the current Set arrays exists or not. [1]
+ * If exists I will increase this temporary variable and repeat above process [1].
+ * [**] Compare previous result and the current.
+ * DONE.
+ */
